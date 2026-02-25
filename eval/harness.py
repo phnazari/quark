40a53fa202be76e6eed0d6fb2cd5821faeb00219
pnazari <pnazari@student.ethz.ch>
2026-02-25 09:32:49 +0100

    updated dtype

--- eval/harness.py
+++ eval/harness.py
@@ -129,7 +129,7 @@ class QuarkLM(HFLM):
         else:
             model = model_wrapper
 
-        model.to(device)
+        model.to(device).bfloat16()
         model.eval()
 
         # 2. Initialize HFLM
